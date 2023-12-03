2850ad5cd7fe0ffbfb541c366881cbe360287c7c
beleir <beleir.messay.@a2sv.org>
2023-12-03 12:49:46 +0300

    comitted
    Signed-off-by: beleir101 <beleirmessay2436@gmail.com>

---
+++ Easy/Longest Common Prefix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        lon = ""
+        strs.sort(key = lambda x:len(x))
+        for i in range(len(strs[0])):
+            for x in strs:
+                if x[i] != strs[0][i]:
+                    return lon
+            lon += strs[0][i]
+        return lon
