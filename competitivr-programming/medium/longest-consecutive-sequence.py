89e42749a6f799661d7645c9d648d5b196471700
beleir <152776084+beleir@users.noreply.github.com>
2023-12-14 22:43:15 +0300

    Added solution for Longest Consecutive Sequence

---
+++ competitivr-programming/medium/longest-consecutive-sequence.py
@@ -0,0 +1,14 @@
+#couldnt do it in a rel time pasted from the solution
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest = 0
+        num_set = set(nums)
+
+        for n in num_set:
+            if (n-1) not in num_set:
+                length = 1
+                while (n+length) in num_set:
+                    length += 1
+                longest = max(longest, length)
+        
+        return longest
