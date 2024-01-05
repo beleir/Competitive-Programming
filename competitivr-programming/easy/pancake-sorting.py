5f881eb78ff42d039a98f72e324455ed8472c87f
beleir <152776084+beleir@users.noreply.github.com>
2024-01-05 21:24:07 +0300

    Added solution for Pancake Sorting

--- competitivr-programming/easy/pancake-sorting.py
+++ competitivr-programming/easy/pancake-sorting.py
@@ -1,32 +1,21 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        flippers = []
-        for i in range(len(arr)):
-            #print(arr,flippers)
+        outs = []
+        p = 0
+        while p < len(arr):
+            v = max(arr[:len(arr)-p])
+            pn = arr.index(v)
+            outs.append(pn+1)
+            vs = 0 
+            while vs < ((pn+1)//2):
+                arr[vs],arr[pn-vs] = arr[pn-vs], arr[vs]
+                vs += 1
             if arr == sorted(arr):
-                return flippers
-            max = (0,0)
-            for j in range(len(arr)-i):
-                if max[0] < arr[j]:
-                    max = (arr[j],j)
-            if max[1] != 0:
-                flippers.append(max[1]+1)
-                c = arr[:max[1]+1]
-                c.reverse()
-                arr = c + arr[max[1]+1 : ]
-                flippers.append(len(arr)-i)
-                if i == 0:
-                    arr.reverse()
-                else:
-                    d = arr[:len(arr)-i]
-                    d.reverse()
-                    arr = d + arr[len(arr)-i:]
-            else:
-                flippers.append(len(arr)-i)
-                if i == 0:
-                    arr.reverse()
-                else:
-                    d = arr[:len(arr)-i]
-                    d.reverse()
-                    arr = d + arr[len(arr)-i:]
-        return flippers
+                break
+            outs.append(len(arr)-p)
+            pr = 0
+            while pr < ((len(arr)-p)//2):
+                arr[pr],arr[len(arr)-1-p-pr] = arr[len(arr)-1-p-pr], arr[pr]
+                pr += 1
+            p += 1
+        return outs
